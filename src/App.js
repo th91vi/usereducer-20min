a3856648b8b134a19c6e42b999032a5bccdaaf66
Thiago Vieira <th91v@outlook.com>
2020-07-04 16:18:44 -0300

    estado inicial da aplicação

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+function App() {
+    const [ count, setCount ] = useState(0)
+
+    function increment() {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function decrement() {
+        setCount(prevCount => prevCount - 1)
+    }
+
+    return (
+        <>
+         <button onClick={decrement}>-</button>   
+         <span> { count } </span>
+         <button onClick={increment}>+</button>   
+        </>
+    )
+}
+
+export default App
